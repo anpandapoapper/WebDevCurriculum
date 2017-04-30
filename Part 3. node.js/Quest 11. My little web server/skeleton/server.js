8764116545bbd2b01ea45241cbe966e0661ce5da
anpanda <anpanda@postech.ac.kr>
2017-05-01 01:20:02 +0900

    clear Quest 12

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -1,5 +1,14 @@
 var http = require('http');
-
-http.createServer(function(req, res) {
+var url = require('url');
+var querystring = require('querystring');
 	// TODO: 이 곳을 채워넣으세요..!
-}).listen(8080);
+http.createServer(function(req, res) {
+	var parsedUrl = url.parse(req.url);
+	var parsedQuery = querystring.parse(parsedUrl.query);
+	console.log(parsedQuery);
+
+	if ((req.method == 'GET' || req.method == 'POST' ) && parsedUrl.pathname == '/foo') {
+		console.log("Hello, " + parsedQuery["bar"]);
+	}
+	res.end("<html><body>광진?</body></html>");
+}).listen(8027);
